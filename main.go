3606b8bd7ec69d35b9bef73e1466ff4afe2b9f8e
Héctor Domínguez <hdomin@gmail.com>
2023-04-01 07:20:25 +0000

    Implementando https

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"crypto/tls"
+	"log"
 	"net/http"
 
 	"jwtgo/authentication"
@@ -12,5 +14,21 @@ func main() {
 	mux.HandleFunc("/login", authentication.Login)
 	mux.HandleFunc("/validate", authentication.ValidateToken)
 
-	http.ListenAndServe(":8080", mux)
+	certFile := "./keys/cert.pem"
+	keyFile := "./keys/key.pem"
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+		TLSConfig: &tls.Config{
+			// Load the SSL/TLS certificate and private key
+			Certificates: []tls.Certificate{},
+		},
+	}
+
+	err := server.ListenAndServeTLS(certFile, keyFile)
+	if err != nil {
+		log.Fatal("ListenAndServeTLS: ", err)
+	}
+
 }
